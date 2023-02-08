2af4d2807fb1f7dda16e064b406ef2a3c0333098
Rabiya Butt <air.bay.x@gmail.com>
2023-02-08 10:22:43 +0500

    update output function

--- codePlayer/js/main.js
+++ codePlayer/js/main.js
@@ -1,1 +1,6 @@
 
+function updateOutput(){
+    $("iframe").contents().find("html").html("<html><head><style type='text/css'>" + $("#cssDisplay").val() + "</style></head><body>" + $("#htmlDisplay").val() + "</body></html>");
+
+    document.getElementById("outputDisplay").contentWindow.eval($("#jsDisplay").val());
+};
